38636c7f5537ea3003503463c18a3bd4f7ab04a5
Vicky <vickykotak09@gmail.com>
2025-09-26 16:10:25 +0530

    single input check

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ function add(str) {
   if (str == "") return 0;
   let sum = 0;
 
+  for (let i = 0; i < str; i++) {
+    sum += parseInt(str[i]);
+  }
+
   return sum;
 }
 
-let str = "";
-console.log(add(str));
+let testCase = [
+  "", //0
+  "1", //1
+];
+
+testCase.forEach((str) => console.log(add(str)));
 
 module.exports = add;
--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const add = require("./index");
 test("pass empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns number for single number input", () => {
+  expect(add("1")).toBe(1);
+});
