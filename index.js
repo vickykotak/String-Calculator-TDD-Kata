6628314b2468f7708f659c8972d1eac9ed0e30e8
Vicky <vickykotak09@gmail.com>
2025-09-26 16:13:52 +0530

    more then one numbers

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function add(str) {
   if (str == "") return 0;
   let sum = 0;
 
-  for (let i = 0; i < str; i++) {
-    sum += parseInt(str[i]);
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) sum += parseInt(str[i]);
   }
 
   return sum;
@@ -12,6 +12,8 @@ function add(str) {
 let testCase = [
   "", //0
   "1", //1
+  "1,5", //6
+  "1,2,3,4", //10
 ];
 
 testCase.forEach((str) => console.log(add(str)));
--- index.test.js
+++ index.test.js
@@ -7,3 +7,11 @@ test("pass empty string", () => {
 test("returns number for single number input", () => {
   expect(add("1")).toBe(1);
 });
+
+test("returns sum for two numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
+
+test("handles multiple numbers", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
